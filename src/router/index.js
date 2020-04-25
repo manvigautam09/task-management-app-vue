77622c767500614bef1d5827f398650dabdffd05
ManviSharma09 <manvis@geekyants.com>
2020-04-25 14:10:33 +0530

    mader route constants

--- src/router/index.js
+++ src/router/index.js
@@ -3,24 +3,25 @@ import Router from "vue-router";
 import Home from "../Views/Home"
 import Login from "../Views/Login";
 import SignUp from "../Views/SignUp";
+import routeConstants from "./pathConstants";
 
 Vue.use(Router);
 
 export default new Router({
     routes: [
         {
-            path: "/",
-            name: "Home",
+            path: routeConstants.HOME.path,
+            name: routeConstants.HOME.name,
             component: Home
         },
         {
-            path: "/login",
-            name: "Login",
+            path: routeConstants.LOGIN.path,
+            name: routeConstants.LOGIN.name,
             component: Login
         },
         {
-            path: "/signUp",
-            name: "SignUp",
+            path: routeConstants.SIGNUP.path,
+            name: routeConstants.SIGNUP.name,
             component: SignUp
         }
     ]
---
+++ src/router/pathConstants.js
@@ -0,0 +1,15 @@
+const routeConstants = {
+    HOME: {
+        path: "/",
+        name: "Home"
+    },
+    LOGIN: {
+        path: "/login",
+        name: "Login",
+    },
+    SIGNUP: {
+        path: "/signUp",
+        name: "SignUp",
+    }
+}
+export default routeConstants;
